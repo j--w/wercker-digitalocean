1b8c43c7059ef5417443d0dba28764208c690040
Jim <jw@jimbo.io>
2014-03-27 19:43:21 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+	res.send('Hello duudde!');
+});
+
+app.get('/stuff.json', function(req, res) {
+	res.writeHead(200, {'Content-Type': 'application/json'});
+	res.write(JSON.stringify({stuff: ['a', 'b', 'c']}));
+	res.end();
+});
+
+module.exports = app;
